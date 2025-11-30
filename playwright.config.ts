64e928a210d19c5f08ec2fdd046e8d5be061d12b
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-11-30 22:20:52 +0000

    add playwright tests to the workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://pst-web.toolshop.svc.cluster.local',
+    // baseURL: 'http://pst-web.toolshop.svc.cluster.local',
+    baseURL: 'http://127.0.0.1:8080/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
